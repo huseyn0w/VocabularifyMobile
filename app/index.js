978dc81a25e1bbac46be10af244f1c1940531d8c
Elman Huseynov <thehuseyn0w@gmail.com>
2024-08-03 21:59:19 +0200

    Add tab bar icons

--- app/index.js
+++ app/index.js
@@ -3,7 +3,8 @@ import { registerRootComponent } from 'expo';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createStackNavigator, TransitionSpecs, CardStyleInterpolators } from '@react-navigation/stack';
+import { Ionicons } from '@expo/vector-icons'; // Import the Ionicons component
 import HomeScreen from './screens/HomeScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import ModeScreen from './screens/ModeScreen';
@@ -15,7 +16,15 @@ const Stack = createStackNavigator();
 
 const SettingsStack = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        transitionSpec: {
+          open: TransitionSpecs.TransitionIOSSpec,
+          close: TransitionSpecs.TransitionIOSSpec,
+        },
+        cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+      }}
+    >
       <Stack.Screen name="SettingsScreen" component={SettingsScreen} options={{ title: 'Settings' }} />
       <Stack.Screen name="ModeScreen" component={ModeScreen} options={{ title: 'Mode' }} />
       <Stack.Screen name="LanguageScreen" component={LanguageScreen} options={{ title: 'Language' }} />
@@ -27,7 +36,25 @@ const SettingsStack = () => {
 const App = () => {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ color, size }) => {
+            let iconName;
+
+            if (route.name === 'Main') {
+              iconName = 'home';
+            } else if (route.name === 'Settings') {
+              iconName = 'settings';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: [{ display: 'flex' }, null],
+        })}
+      >
         <Tab.Screen name="Main" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsStack} options={{ headerShown: false }} />
       </Tab.Navigator>
